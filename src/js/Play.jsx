33dd2357c709f644f1a27921014379423ae11202
amiraqeel <amir.aqeel123@gmail.com>
2020-07-01 23:43:23 +0530

    second commit

--- src/js/Play.jsx
+++ src/js/Play.jsx
@@ -10,32 +10,36 @@ class Play extends React.Component {
             score: 0,
             level: 1,
             display: "initial",
-            animationSpeed: 2000,
+            animationSpeed: 2500,
             brickSpeed: 1000,
-            height: 0
+            height: 0,
+            top: 0
         };
         this.brick = "";
     }
     componentDidMount() {
         if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-            $("#area").width("25%");
+            $("#area").width("30%");
         }
         $("#area").css("height", window.innerHeight - 1);
+        $("#rect0,#rect1,#rect2").css({ "visibility": "hidden", "height": window.innerHeight / 4 });
         $("#rect0,#rect1,#rect2").width($("#area").width() / 3);
-        $("#rect0,#rect1,#rect2").css("height", window.innerHeight / 4);
-        $("#rect0,#rect1,#rect2").css("visibility", "hidden");
-        this.setState({ height: $("#area").height() - $("#rect0").height() - $("#stats").height() - 1 }, () => { console.log("height"); });
+        $("#rect0,#rect1,#rect2").css("top", 0 - 2 * $("#stats").height() - $("#rect0").height());
+        this.setState({
+            height: $("#area").height() - $("#rect0").height() - $("#stats").height() + 2,
+            top: 0 - 2 * $("#stats").height() - $("#rect0").height()
+        }, () => { console.log(this.state.top); });
         $('#myModal').modal({
             backdrop: 'static',
             keyboard: false,
             show: false
         });
-        $("#area").on('click', (event) => {
+        $("#area").on('click tap', (event) => {
             if (event.target.id === "area")
                 this.end();
             else return;
         });
-        $("#rect0,#rect1,#rect2").on('click', (event) => {
+        $("#rect0,#rect1,#rect2").on('click tap', (event) => {
             this.hideBrick(event);
         });
         this.brick = setInterval(this.showBrick, this.state.brickSpeed);
@@ -57,7 +61,7 @@ class Play extends React.Component {
         let id = event.currentTarget.id;
         $(`#${id}`).css("visibility", "hidden");
         $(`#${id}`).stop(true);
-        $(`#${id}`).animate({ top: "0vh" }, 1);
+        $(`#${id}`).animate({ top: this.state.top }, 0.1);
         this.setState((state) => {
             return { score: ++state.score };
         }, () => {
@@ -89,7 +93,7 @@ class Play extends React.Component {
         }
         return <div id="area" className="bg-success border border-dark rounded-lg mx-auto">
             <div className="d-flex flex-column">
-                <div id="stats" className="d-flex flex-row bg-dark text-monospace text-light justify-content-around">
+                <div id="stats" className="d-flex flex-row bg-dark text-monospace text-light justify-content-around mt-n1">
                     <div id="score" className="flex-fill mr-5 ml-3">SCORE : 0</div>
                     <div id="level" className="flex-fill ml-4">LEVEL : 1</div>
                 </div>
